b10d5b75465f242adce49b7945b3fef03e7b6aab
Akhmad Dias <2ahmad86@gmail.com>
2021-01-11 00:07:15 +0600

    and more...

--- src/components/Delivery.vue
+++ src/components/Delivery.vue
@@ -4,9 +4,6 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Наше местоположение: Ауезова, 37
-          </h1>
-          <h1 class="title">
             График работы доставки:
           </h1>
           <h2 class="subtitle">
@@ -21,14 +18,15 @@
             </li>
           </ul>
           <h1 class="title">
-            Оплата:
+            Стоимость доставки:
           </h1>
           <h2 class="subtitle">Доставка в радиусе 3 км - 500тг</h2>
           <h2 class="subtitle">В радиусе 5 км - 700тг</h2>
           <h2 class="subtitle">Больше 5 км - 1000тг</h2>
-          <h2 class="subtitle">
-            Точная цена доставки будет уточнена после оформления заказа
-          </h2>
+          <h1 class="title">
+            Наше местоположение: Ауезова, 37
+          </h1>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1453.1041694204027!2d76.90140085810515!3d43.24705524852309!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3883694767099875%3A0x50e3caa70518f6d8!2z0YPQu9C40YbQsCDQkNGD0Y3Qt9C-0LLQsCAzNywg0JDQu9C80LDRgtGLIDA1MDAwMA!5e0!3m2!1sru!2skz!4v1593433691563!5m2!1sru!2skz" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
           <ul>
             <li>
               Наличный/Безналичный расчет для юридических лиц
@@ -36,14 +34,10 @@
             <li>
               Оплата при доставки курьером для физических лиц осуществляется наличными
             </li>
+            <li>
+              Точная цена доставки будет уточнена после оформления заказа
+            </li>
           </ul>
-        </div>
-      </div>
-    </section>
-    <section class="section">
-      <div class="columns">
-        <div class="column">
-          
         </div>
       </div>
     </section>
